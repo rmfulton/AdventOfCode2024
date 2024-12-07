38d0d74a370d36e8681dad45e190fc1b37c3018f
rmfulton <rmfulton@gmail.com>
2024-12-07 12:19:14 -0800

    solved part 1

--- Day06/solution1.cpp
+++ Day06/solution1.cpp
@@ -9,129 +9,144 @@
 
 using namespace std;
 
-vector<int> parseConstraint(string line)
-{
-    for (int i = 0; i < line.size(); ++i)
-    {
-        if (line[i] == '|')
-        {
-            line[i] = ' ';
-        }
-    }
-    istringstream ss(line);
-    vector<int> constraint;
-    int x;
-    for (int i = 0; i < 2; ++i)
-    {
-        ss >> x;
-        constraint.push_back(x);
-    }
-    return constraint;
-}
-
-vector<int> parsePage(string line)
-{
-    for (int i = 0; i < line.size(); ++i)
-    {
-        if (line[i] == ',')
-        {
-            line[i] = ' ';
-        }
-    }
-    istringstream ss(line);
-    vector<int> page;
-    for (int x; ss >> x;)
-        page.push_back(x);
-    return page;
-}
-
-vector<vector<vector<int> > > parseInput(ifstream *infile)
-{
-    string line = "|";
-    vector<vector<int> > constraints;
-    vector<vector<int> > pages;
-    while (*infile >> line)
-    {
-        if (line.find("|") != string::npos)
-        {
-            vector<int> constraint = parseConstraint(line);
-            constraints.push_back(constraint);
-        }
-        else
-        {
-            vector<int> page = parsePage(line);
-            pages.push_back(page);
-        }
-    }
-    vector<vector<vector<int> > > constraintsAndPages;
-    constraintsAndPages.push_back(constraints);
-    constraintsAndPages.push_back(pages);
-    return constraintsAndPages;
-}
-
-bool isGoodPage(vector<int> candidate, vector<vector<int> > constraints)
-{
-    map<int, int> indexOf;
-    for (int i = 0; i < candidate.size(); ++i)
-    {
-        indexOf[candidate[i]] = i;
-    }
-    for (vector<int> c : constraints)
-    {
-        int left = c[0];
-        int right = c[1];
-        if (indexOf.find(left) != indexOf.end())
-        {
-            if (indexOf.find(right) != indexOf.end())
-            {
-                if (indexOf[left] > indexOf[right])
-                {
-                    cout << "candidate: ";
-                    for (int x : candidate)
-                    {
-                        cout << x << " ";
-                    }
-                    cout << " failed on constraint " << left << " " << right << endl;
-                    return false;
-                }
-            }
-        }
-    }
-    cout << "candidate: ";
-    for (int x : candidate)
-    {
-        cout << x << " ";
-    }
-    cout << "passed" << endl;
-    return true;
-}
-
-vector<vector<int> > getGoodPages()
+vector<vector<char> > parseInput()
 {
     string filename = "input.txt";
     ifstream infile(filename);
-    vector<vector<vector<int> > > candP = parseInput(&infile);
-    vector<vector<int> > constraints = candP[0];
-    vector<vector<int> > pages = candP[1];
-    vector<vector<int> > goodPages;
-    for (vector<int> p : pages)
+
+    string target;
+    vector<vector<char> > result;
+    while (infile >> target)
     {
-        if (isGoodPage(p, constraints))
+        vector<char> row;
+        for (char c : target)
         {
-            goodPages.push_back(p);
+            row.push_back(c);
+        }
+        result.push_back(row);
+    }
+    return result;
+}
+
+vector<vector<int> > getInitialPositionAndVelocity(vector<vector<char> > grid, int N, int M)
+{
+    vector<int> p;
+    vector<int> v;
+    vector<vector<int> > both;
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < M; ++j)
+        {
+            switch (grid[i][j])
+            {
+            case '.':
+                break;
+            case '#':
+                break;
+            case '^':
+                p.push_back(i);
+                p.push_back(j);
+                v.push_back(-1);
+                v.push_back(0);
+                both.push_back(p);
+                both.push_back(v);
+                return both;
+            case '>':
+                p.push_back(i);
+                p.push_back(j);
+                v.push_back(0);
+                v.push_back(1);
+                both.push_back(p);
+                both.push_back(v);
+                return both;
+            case 'v':
+                p.push_back(i);
+                p.push_back(j);
+                v.push_back(1);
+                v.push_back(0);
+                both.push_back(p);
+                both.push_back(v);
+                return both;
+            case '<':
+                p.push_back(i);
+                p.push_back(j);
+                v.push_back(0);
+                v.push_back(-1);
+                both.push_back(p);
+                both.push_back(v);
+                return both;
+            }
         }
     }
-    return goodPages;
+    return both;
+}
+
+bool inBounds(vector<int> pos, int N, int M){
+    return 0 <= pos[0] && pos[0] < N && 0 <= pos[1] && pos[1] < M;
+}
+
+vector<vector<int> > getNewConditions(vector<vector<char> >grid, int N, int M, vector<int> pos,vector<int> vel){
+    vector<vector<int> > pair;
+    vector<int> candidate;
+    candidate.push_back(pos[0] + vel[0]);
+    candidate.push_back(pos[1] + vel[1]);
+    if (!inBounds(candidate, N, M))
+    {
+        pair.push_back(candidate);
+        pair.push_back(vel);
+    } 
+    else if (grid[candidate[0]][candidate[1]] == '#')
+    {
+        vector<int> newVel;
+
+        newVel.push_back(vel[1]);
+        newVel.push_back(-vel[0]);
+        pair.push_back(pos);
+        pair.push_back(newVel);
+    }
+    else
+    {
+        pair.push_back(candidate);
+        pair.push_back(vel);
+    }
+    return pair;
 }
 
 int main()
 {
-    vector<vector<int> > goodPages = getGoodPages();
-    int s = 0;
-    for (vector<int> p : goodPages)
+    time_t t1 = time(NULL);
+    vector<vector<char> > grid = parseInput();
+    const int N = grid.size();
+    const int M = grid[0].size();
+    vector<vector<int> > initialConditions = getInitialPositionAndVelocity(grid, N, M);
+    vector<int> pos = initialConditions[0];
+    vector<int> vel = initialConditions[1];
+
+    bool visited[N][M];
+    for (int i = 0; i < N; ++i)
     {
-        s += p[p.size() / 2];
+        for (int j = 0; j < M; ++j)
+        {
+            visited[i][j] = false;
+        }
     }
-    std::cout << s << std::endl;
+    grid[pos[0]][pos[1]] = '.';
+    while (inBounds(pos,N,M)){
+        visited[pos[0]][pos[1]] = true;
+        vector<vector<int> > newConditions = getNewConditions(grid, N, M, pos,vel);
+        pos = newConditions[0];
+        vel = newConditions[1];
+    }
+    
+    int totalVisited = 0;
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < M; ++j)
+        {
+            totalVisited += visited[i][j] ? 1 : 0;
+        }
+    }
+    cout << totalVisited << endl;
+    cout << time(NULL) - t1 << endl;
     return 0;
 }
