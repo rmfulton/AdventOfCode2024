69fd3571e6e0108b393b304e121f4e09428900cd
rmfulton <rmfulton@gmail.com>
2024-12-06 20:10:14 -0800

    solved days 2 and 3

---
+++ Day02/solution2.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+bool isIncreasing(int left, int right, bool increasing){
+    return left < right == increasing;
+}
+bool notTheSame(int left, int right){
+    return left != right;
+}
+bool withinFour(int left, int right){
+    if (left < right){
+        return left + 4 > right;
+    } else {
+        return right + 4 > left;
+    }
+}
+
+bool isValidSequence(std::vector<int> nums){
+    int L = nums.size();
+    if ( L < 2){
+        return true;
+    }
+    bool increasing = nums[0] < nums[1];
+    for (int i = 0; i < L - 1; ++i){
+        int left = nums[i];
+        int right = nums[i+1];
+        bool valid = isIncreasing(left, right, increasing) && notTheSame(left, right) && withinFour(left, right);
+        if (!valid){
+            return false;
+        }
+    }
+    return true;
+
+}
+
+bool isValidExceptForOne(std::vector<int> nums){
+    if (isValidSequence(nums)){
+        return true;
+    }
+    for (int i = 0; i < nums.size(); ++i){
+        std::vector<int> reduced;
+        for (int j = 0; j < nums.size(); ++j){
+            if (j == i){
+                continue;
+            }
+            reduced.push_back(nums[j]);
+        }
+        if (isValidSequence(reduced)){
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+
+    std::string filename = "input.txt";
+    std::ifstream infile(filename);
+
+    std::string line;
+    int goodCount = 0;
+    while (std::getline(infile, line))
+    {
+        std::istringstream iss(line);
+        std::vector<int> nums;
+        int x;
+        while (iss >> x){
+            nums.push_back(x);
+        }
+        if (isValidExceptForOne(nums)){
+            goodCount++;
+        }
+    }
+
+    std::cout << goodCount << std::endl;
+
+    return 0;
+}
---
+++ Day03/solution1.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+#include <vector>
+#include <regex>
+
+std::vector<std::string> parseInput()
+{
+    std::string filename = "input.txt";
+    std::ifstream infile(filename);
+
+    std::string target;
+    std::regex pattern("mul\\(\\d{1,3},\\d{1,3}\\)");
+    std::vector<std::string> result;
+    while (infile >> target)
+    {
+        for (std::smatch sm; std::regex_search(target, sm, pattern);)
+        {
+            result.push_back(sm.str());
+            std::cout << sm.str() << std::endl;
+            target = sm.suffix();
+        }
+        std::cout << std::endl;
+    }
+    return result;
+}
+
+std::vector<std::string> cleanedInput(std::vector<std::string> debrided)
+{
+    std::string prefix = "mul(";
+    std::vector<std::string> cleaned;
+    for (std::string e : debrided)
+    {
+        int start = prefix.size();
+        int end = e.find(",");
+        int length = end - start;
+        std::string first = e.substr(start, length);
+        start = e.find(",") + 1;
+        end = e.size() - 1;
+        length = end - start;
+        std::string second = e.substr(start, length);
+
+        std::string tmp = first + " " + second;
+        std::cout << tmp << std::endl;
+
+        cleaned.push_back(tmp);
+    }
+    std::cout << std::endl;
+    return cleaned;
+}
+
+int main()
+{
+    std::vector<std::string> expressions = parseInput();
+    std::vector<std::string> cleaned = cleanedInput(expressions);
+    int result = 0;
+    for (std::string pair : cleaned)
+    {
+        std::istringstream iss(pair);
+        int a, b;
+        iss >> a >> b;
+        std::cout << a << " " << b << std::endl;
+        result += a * b;
+    }
+    std::cout << result << std::endl;
+    return 0;
+}
