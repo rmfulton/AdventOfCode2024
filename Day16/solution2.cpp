8b61fe0facaf0af5d92078a8f1f4b2fea9bb367f
rmfulton <rmfulton@gmail.com>
2024-12-25 14:41:34 -0800

    solved day 16

--- Day16/solution2.cpp
+++ Day16/solution2.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <map>
 #include <cmath>
+#include <queue>
 
 #include <vector>
 #include <regex>
@@ -11,63 +12,149 @@
 using namespace std;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
-typedef vector<vi> vvi;
-typedef vector<vvi> vvvi;
+typedef vector<vector<int>> vvi;
 typedef vector<vector<char>> vvc;
 typedef pair<vector<vector<char>>, string> pvvcs;
 
-
-pvvcs parseWarehouseAndMovements()
+vector<vector<char>> parseIntoCharArray()
 {
     string filename = "input.txt";
     ifstream infile(filename);
-    vvc warehouse;
+    vector<vector<char>> maze;
     string line;
-    infile >> line;
-    vector<char> firstline;
-    for (char c: line){
-        firstline.push_back('#');
-        firstline.push_back('#');
-    }
-    warehouse.push_back(firstline);
     while (infile >> line)
     {
         vector<char> row;
-        for (char c : line){
-            switch(c){
-                case '.':
-                    row.push_back('.');
-                    row.push_back('.');
-                    break;
-                case '#':
-                    row.push_back('#');
-                    row.push_back('#');
-                    break;
-                case '@':
-                    row.push_back('@');
-                    row.push_back('.');
-                    break;
-                case 'O':
-                    row.push_back('[');
-                    row.push_back(']');
-                    break;
-            }
-        }
-        warehouse.push_back(row);
-        bool should_break = true;
-        for (char c: line){
-            if (c != '#') should_break = false;
-        }
-        if (should_break) break;
+        for (char c : line)
+            row.push_back(c);
+        maze.push_back(row);
     }
-    string movements = "";
-    while (infile >> line){
-        movements += line;
-    }
-    return make_pair(warehouse, movements);
-
+    return maze;
 }
 
+vi dir(int i)
+{
+    switch (i)
+    {
+    case 0:
+        return vi{0, 1};
+    case 1:
+        return vi{-1, 0};
+    case 2:
+        return vi{0, -1};
+    case 3:
+        return vi{1, 0};
+    }
+    throw;
+}
+
+bool improves(vi const &state, int const &proposedValue, map<vi, int> &cost)
+{
+    if (cost.find(state) == cost.end())
+    {
+        return true;
+    }
+    return proposedValue < cost[state];
+}
+
+vi getCoordOf(vvc const &maze, char c){
+    for (int i = 0; i < maze.size(); ++i){
+        for (int j = 0; j < maze[0].size(); ++j){
+            if (maze[i][j] == c) return vi{i,j};
+        }
+    }
+    throw;
+}
+map<vi,int> getMinimalCostFrom(vvc const &maze, vvi const &baseStates){
+
+    int MOVE_COST = 1;
+    int ROTATE_COST = 1000;
+    map<vi, int> cost;
+    queue<vi> stateQ;
+    for(vi baseState: baseStates){
+        cost[baseState] = 0;
+        stateQ.push(baseState);
+    }
+    while (stateQ.size() > 0)
+    {
+        vi state = stateQ.front();
+        stateQ.pop();
+
+        vi rotateState1{state[0], state[1], (state[2] + 1) % 4};
+        vi rotateState2{state[0], state[1], (state[2] + 3) % 4};
+        vi delta = dir(state[2]);
+        vi translateState{state[0] + delta[0], state[1] + delta[1], state[2]};
+        char c = maze[translateState[0]][translateState[1]];
+        if (c == '.' || c == 'E' || c == 'S')
+        {
+            int proposedCost = MOVE_COST + cost[state];
+            if (improves(translateState, proposedCost, cost))
+            {
+                stateQ.push(translateState);
+                cost[translateState] = proposedCost;
+            }
+        }
+        int proposedCost = ROTATE_COST + cost[state];
+        for (vi rotateState : vvi{rotateState1, rotateState2})
+        {
+            if (improves(rotateState, proposedCost, cost))
+            {
+                stateQ.push(rotateState);
+                cost[rotateState] = proposedCost;
+            }
+        }
+    }
+    return cost;
+}
+
+map<vi,int> getCostFromStart(vvc const &maze){
+    const vi startCoords = getCoordOf(maze, 'S');
+    const vi begin{startCoords[0], startCoords[1], 0};
+    const map<vi, int> costFromStart = getMinimalCostFrom(maze, vvi{begin});
+    return costFromStart;
+}
+
+map<vi,int> getCostFromEnd(vvc const &maze){
+    const vi endCoords = getCoordOf(maze, 'E');
+    vvi endStates;
+    for (int i = 0; i < 4; ++i){
+        vi endState{endCoords[0], endCoords[1], i};
+        endStates.push_back(endState);
+    }
+    const map<vi,int> costFromEnd = getMinimalCostFrom(maze, endStates);
+    return costFromEnd;
+}
+
+int findNumberOfGoodPoints(map<vi,int> const &costFromStart, map<vi,int> const &costFromEnd, vvc const &maze){
+    const vi startCoords = getCoordOf(maze, 'S');
+    vi reversedStartCoords{startCoords[0], startCoords[1], 2};
+    int bestCost = costFromEnd.at(reversedStartCoords);
+    int numPointsWithOptimalPath = 0;
+    for (int i = 0; i < maze.size(); ++i){
+        for (int j = 0; j < maze.size(); ++j){
+            bool shouldIncrementNumPoints = false;
+            for (int k = 0; k < 4; ++k){
+                vi stateFromBegin{i,j,k};
+                vi stateFromEnd{i,j,(k+2)%4};
+                if (costFromStart.find(stateFromBegin) != costFromStart.end()){
+                    int totalCost = costFromStart.at(stateFromBegin) + costFromEnd.at(stateFromEnd);
+                    if (totalCost == bestCost){
+                        shouldIncrementNumPoints = true;
+                    }
+                }
+            }
+            if (shouldIncrementNumPoints) ++numPointsWithOptimalPath;
+        }
+    }
+    return numPointsWithOptimalPath;
+}
+
+int solve(vvc const &maze)
+{
+    const map<vi,int> costFromStart = getCostFromStart(maze);
+    const map<vi,int> costFromEnd = getCostFromEnd(maze);
+    return findNumberOfGoodPoints(costFromStart, costFromEnd, maze);
+}
 void printBoard(const vvc &board){
     for (vector<char> line: board){
         for (char c: line){
@@ -77,154 +164,16 @@ void printBoard(const vvc &board){
     }
 }
 
-vvc copyBoard(const vvc &b){
-    vvc newBoard;
-    for (vector<char> v : b){
-        vector<char> newv;
-        for (char c: v){
-            newv.push_back(c);
-        }
-        newBoard.push_back(newv);
-    }
-    return newBoard;
-}
-
-vi getRobotCoords(const vvc &board){
-    for (int i = 0; i < board.size(); ++i){
-        for (int j = 0; j < board[0].size(); ++j){
-            if (board[i][j] == '@') return vi{i,j};
-        }
-    }
-    return vi{-1,-1};
-}
-
-int computeHash(vvc currentState){
-    int result = 0;
-    for (int i = 0; i < currentState.size(); ++i){
-        for (int j = 0; j < currentState[0].size(); ++j){
-            if (currentState[i][j] == '['){
-                result += 100*i + j;
-            }
-        }
-    }
-    return result;
-}
-
-bool containsWall(vvi const &candidateCoords, vvc const &currentState){
-    for (vi coord: candidateCoords){
-        if (currentState[coord[0]][coord[1]] == '#') return true;
-    }
-    return false;
-}
-
-bool allSpace(const vvi &candidateCoords, const vvc &currentState){
-    for (vi coord: candidateCoords){
-        if (currentState[coord[0]][coord[1]] != '.') return false;
-    }
-    return true;
-}
-
-vi dir(char c){
-    switch (c){
-        case '<':
-            return vi{0,-1};
-        case '>':
-            return vi{0,1};
-        case 'v':
-            return vi{1,0};
-        case '^':
-            return vi{-1,0};
-    }
-    return {10000000, 10000000};
-}
-
-vvi getNextLayer(const vvc &currentState, vvi currentLayer, char c){
-    map<vi, bool> nextLayerSet;
-    vector<int> delta = dir(c);
-    for(vi element: currentLayer){
-        vi destination{element[0] + delta[0], element[1] + delta[1]};
-        char v = currentState[destination[0]][destination[1]];
-        if (v == '.') continue;
-        if (v == '#'){
-            nextLayerSet[destination] = true;
-            break;
-        }
-        if (v == ']'){
-            nextLayerSet[destination] = true;
-            if (c == '^' || c == 'v'){
-                nextLayerSet[vi{destination[0], destination[1] - 1}] = true;
-            }
-        }
-        if (v == '['){
-            nextLayerSet[destination] = true;
-            if (c == '^' || c == 'v'){
-                nextLayerSet[vi{destination[0], destination[1] + 1}] = true;
-            }
-        }
-    }
-    vvi nextLayer;
-    for (auto const &x: nextLayerSet){
-        nextLayer.push_back(x.first);
-    }
-    return nextLayer;
-}
-
 /*
-High Level:
-- Phase I (while true) [Create a stack of layers]
-- get the next layer of coords
-- if the layer contains an obstacle,
-    - return the previous state
-- if the layer is "....", break
-- Phase II (while stack.size() > 0)
--   pop the most recent layer
--   move it to where it belongs
--   replace it with empty space
-- return the final state
-
-*/
-vvc updateState(const vvc &currentState, char c){
-    vi startCoords = getRobotCoords(currentState);
-    vvvi layerStack;
-    layerStack.push_back(vvi{startCoords});
-    while (true){
-        vvi nextLayer = getNextLayer(currentState, layerStack.back(), c);
-        if (containsWall(nextLayer, currentState)) return currentState;
-
-        if (allSpace(nextLayer, currentState)) break;
-
-        layerStack.push_back(nextLayer);
-    }
-
-    vvc newState = copyBoard(currentState);
-    vi delta = dir(c);
-    while (layerStack.size() > 0){
-        vvi layer = layerStack.back();
-        layerStack.pop_back();
-        for (vi coord: layer){
-            newState[coord[0] + delta[0]][coord[1] + delta[1]] = currentState[coord[0]][coord[1]];
-            newState[coord[0]][coord[1]] = '.';
-        }
-    }
-    return newState;
-}
-
-/*
-Now that things got fat, should we model continue to model it as a char grid?
-when you push, there is now a set of coordinates that move in the direction
+Now, we need t
 */
 int main()
 {
     time_t t1 = time(NULL);
-    const pvvcs warehouseAndMovements = parseWarehouseAndMovements();
-    vvc warehouse = warehouseAndMovements.first;
-    string movements = warehouseAndMovements.second;
-    vvc currentState = copyBoard(warehouse);
-    for (char c: movements){
-        currentState = updateState(currentState, c);
-        // printBoard(currentState);
-    }
-    cout << computeHash(currentState) << endl;
+    vvc maze = parseIntoCharArray();
+    int result = solve(maze);
+    cout << result << endl;
+    cout << endl;
     cout << time(NULL) - t1 << endl;
     return 0;
 }
