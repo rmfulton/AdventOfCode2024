16ec47b961d8cc4633ddcfa39e9fddc132d97109
rmfulton <rmfulton@gmail.com>
2024-12-29 15:17:13 -0800

    update

--- Day17/solution2.cpp
+++ Day17/solution2.cpp
@@ -12,167 +12,113 @@
 using namespace std;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
+typedef vector<long> vl;
 typedef vector<vector<int>> vvi;
 typedef vector<vector<char>> vvc;
 typedef pair<vector<vector<char>>, string> pvvcs;
 
-vector<vector<char>> parseIntoCharArray()
-{
-    string filename = "input.txt";
-    ifstream infile(filename);
-    vector<vector<char>> maze;
-    string line;
-    while (infile >> line)
-    {
-        vector<char> row;
-        for (char c : line)
-            row.push_back(c);
-        maze.push_back(row);
+
+vi mySpecificProgram(long x){
+    long alpha = x;
+    long beta = 0;
+    long gamma = 0;
+    vi output;
+    while(true){
+        beta = (alpha % 8); // 2 4
+        beta = beta ^ 5; // 1 5
+        gamma = alpha / (1<<beta); // 7 5
+        beta = beta ^ 6; // 1 6
+        alpha = alpha / 8; // 0 3
+        beta = beta ^ gamma; // 4 2
+        output.push_back(beta % 8); // 5 5
+        if (alpha == 0) break; // 3 0
     }
-    return maze;
+    return output;
 }
 
-vi dir(int i)
-{
-    switch (i)
-    {
-    case 0:
-        return vi{0, 1};
-    case 1:
-        return vi{-1, 0};
-    case 2:
-        return vi{0, -1};
-    case 3:
-        return vi{1, 0};
+long toBase10(string binary){
+    long x = 0;
+    for (char c: binary){
+        x = x << 1;
+        x += c == '1' ? 1 : 0;
     }
-    throw;
+    return x;
 }
 
-bool improves(vi const &state, int const &proposedValue, map<vi, int> &cost)
-{
-    if (cost.find(state) == cost.end())
-    {
-        return true;
+string toBinary(long x){
+    if (x == 0) return "0";
+    string out = "";
+    while (x > 0){
+        out = to_string(x % 2) + out;
+        x /= 2;
     }
-    return proposedValue < cost[state];
+    return out;
 }
 
-vi getCoordOf(vvc const &maze, char c){
-    for (int i = 0; i < maze.size(); ++i){
-        for (int j = 0; j < maze[0].size(); ++j){
-            if (maze[i][j] == c) return vi{i,j};
-        }
+void testBinaryFunctions(){
+    long x = 3;
+    if (toBinary(x) != "11") throw;
+    for (int i = 0; i < 1000; ++i){
+        if (toBase10(toBinary(i)) != i) throw;
     }
-    throw;
+    cout << "success";
 }
-map<vi,int> getMinimalCostFrom(vvc const &maze, vvi const &baseStates){
 
-    int MOVE_COST = 1;
-    int ROTATE_COST = 1000;
-    map<vi, int> cost;
-    queue<vi> stateQ;
-    for(vi baseState: baseStates){
-        cost[baseState] = 0;
-        stateQ.push(baseState);
+void printVector(vi vector){
+    for (int x : vector){
+        cout << x << " ";
     }
-    while (stateQ.size() > 0)
-    {
-        vi state = stateQ.front();
-        stateQ.pop();
-
-        vi rotateState1{state[0], state[1], (state[2] + 1) % 4};
-        vi rotateState2{state[0], state[1], (state[2] + 3) % 4};
-        vi delta = dir(state[2]);
-        vi translateState{state[0] + delta[0], state[1] + delta[1], state[2]};
-        char c = maze[translateState[0]][translateState[1]];
-        if (c == '.' || c == 'E' || c == 'S')
-        {
-            int proposedCost = MOVE_COST + cost[state];
-            if (improves(translateState, proposedCost, cost))
-            {
-                stateQ.push(translateState);
-                cost[translateState] = proposedCost;
-            }
-        }
-        int proposedCost = ROTATE_COST + cost[state];
-        for (vi rotateState : vvi{rotateState1, rotateState2})
-        {
-            if (improves(rotateState, proposedCost, cost))
-            {
-                stateQ.push(rotateState);
-                cost[rotateState] = proposedCost;
-            }
-        }
+    cout << '\n';
+}
+string repeat(string x, int times){
+    string out;
+    for (int i = 0; i < times; ++i){
+        out += x;
     }
-    return cost;
+    return out;
 }
-
-map<vi,int> getCostFromStart(vvc const &maze){
-    const vi startCoords = getCoordOf(maze, 'S');
-    const vi begin{startCoords[0], startCoords[1], 0};
-    const map<vi, int> costFromStart = getMinimalCostFrom(maze, vvi{begin});
-    return costFromStart;
-}
-
-map<vi,int> getCostFromEnd(vvc const &maze){
-    const vi endCoords = getCoordOf(maze, 'E');
-    vvi endStates;
-    for (int i = 0; i < 4; ++i){
-        vi endState{endCoords[0], endCoords[1], i};
-        endStates.push_back(endState);
-    }
-    const map<vi,int> costFromEnd = getMinimalCostFrom(maze, endStates);
-    return costFromEnd;
-}
-
-int findNumberOfGoodPoints(map<vi,int> const &costFromStart, map<vi,int> const &costFromEnd, vvc const &maze){
-    const vi startCoords = getCoordOf(maze, 'S');
-    vi reversedStartCoords{startCoords[0], startCoords[1], 2};
-    int bestCost = costFromEnd.at(reversedStartCoords);
-    int numPointsWithOptimalPath = 0;
-    for (int i = 0; i < maze.size(); ++i){
-        for (int j = 0; j < maze.size(); ++j){
-            bool shouldIncrementNumPoints = false;
-            for (int k = 0; k < 4; ++k){
-                vi stateFromBegin{i,j,k};
-                vi stateFromEnd{i,j,(k+2)%4};
-                if (costFromStart.find(stateFromBegin) != costFromStart.end()){
-                    int totalCost = costFromStart.at(stateFromBegin) + costFromEnd.at(stateFromEnd);
-                    if (totalCost == bestCost){
-                        shouldIncrementNumPoints = true;
-                    }
-                }
-            }
-            if (shouldIncrementNumPoints) ++numPointsWithOptimalPath;
-        }
-    }
-    return numPointsWithOptimalPath;
-}
-
-int solve(vvc const &maze)
-{
-    const map<vi,int> costFromStart = getCostFromStart(maze);
-    const map<vi,int> costFromEnd = getCostFromEnd(maze);
-    return findNumberOfGoodPoints(costFromStart, costFromEnd, maze);
-}
-void printBoard(const vvc &board){
-    for (vector<char> line: board){
-        for (char c: line){
-            cout << c;
-        }
-        cout << '\n';
-    }
-}
-
 /*
-Now, we need t
+so here we're just computing
+modulus = value % 8;
+gamma = value / (1 << modulus ^ 5);
+output = (modulus ^ gamma ^ 3) % 8;
+FACT:
+given the first 10 digits of any binary number,
+you can determine what the first output will be (the same as if it was just those 10 digits)
+
+FACT:
 */
+
+void printOutputData(){
+    for (int i = 0; i < 8*8; ++i){
+        string binary = toBinary(i);
+        binary =  repeat("0",6 - binary.size()) + binary;
+        cout << "Input = " << binary << " -> ";
+        printVector(mySpecificProgram(i));
+    }
+}
+
+vi getFirstOutputMap(){
+    int twoToTheEleven = 2048;
+    vector<int> firstOutput;
+    for (int i = 0; i < twoToTheEleven; ++i){
+        vi output = mySpecificProgram(i);
+        firstOutput.push_back(output[0]);
+    }
+    return firstOutput;
+}
+
 int main()
 {
     time_t t1 = time(NULL);
-    vvc maze = parseIntoCharArray();
-    int result = solve(maze);
-    cout << result << endl;
+    // pair<vi,vi> registersProgram = parseInput();
+    // vl registers{registersProgram.first[0], registersProgram.first[1], registersProgram.first[2]};
+    // vi program = registersProgram.second;
+    // testMySpecificProgram(registers, program);
+    // testBinaryFunctions();
+    // printOutputData();
+    testOutputTheory();
+
     cout << endl;
     cout << time(NULL) - t1 << endl;
     return 0;
